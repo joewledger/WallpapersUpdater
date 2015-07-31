d17972a8b715cae97c475b148e26f8156ce85c72
joewledger <joewledger@gmail.com>
2015-07-30 20:41:59 -0400

    Added argument parsing.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- wallpapers.py
+++ wallpapers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import os
 import praw
 import random
@@ -7,6 +7,7 @@ import datetime
 from enum import Enum
 import itertools
 import time
+import argparse
 
 
 class Status(Enum):
@@ -17,17 +18,28 @@ class Status(Enum):
     failed_download = 4
     no_suitable_url = 5
     enter_control = 6
+    bad_arguments = 7
 
 def main():
-    savedir = "/home/joe/Pictures/Wallpapers"
-    subreddits = ['earthporn','minimalwallpaper','wallpapers']
-    file_extensions = ['.png','.jpg']
-    num_tries = 0
 
-    status = Status.enter_control
-    image_uri = None
+    parser = argparse.ArgumentParser(description="An automatic desktop wallpaper updater.")
+    parser.add_argument('savedir',type=str,help="The directory to save wallpapers and log to.")
+    parser.add_argument('--subreddits',nargs='+',type=str,help='The subreddits to scrape images from.')
+    parser.add_argument('--extensions',nargs='+',type=str,help='The allowed file extensions.')
 
-    image_url ,status = find_wallpaper(subreddits,file_extensions)
+    parser.set_defaults(subreddits=['earthporn','minimalwallpaper','wallpapers'],extensions=['.png','.jpg'])
+
+    try:
+        args = parser.parse_args()
+        savedir = args.savedir
+        subreddits = args.subreddits
+        file_extensions = args.extensions
+        status = Status.enter_control
+    except:
+        status = Status.bad_arguments
+
+    if(status == Status.enter_control):
+        image_url ,status = find_wallpaper(subreddits,file_extensions)
     if(status == Status.selected_url):
         image_uri,status = download_wallpaper(image_url,savedir)
     if(status == Status.finished_download):
@@ -55,18 +67,6 @@ def download_wallpaper(url,savedir):
         return save_location, Status.finished_download
     except:
         return save_location, Status.failed_download
-	
-def log(image_uri,savedir,status):
-    log_file = "%s/log.txt" % savedir
-    log_flag = "a" if os.path.isfile(log_file) else "w"
-    writer = open(log_file,log_flag)
-    status_message = "Something unexpected happened, please try again."
-    if(status == Status.changed_wallpaper): status_message = "The wallpaper has been changed successfully."
-    elif(status == Status.unchanged_wallpaper): status_message = "The wallpaper downloaded successfully but could not be set"
-    elif(status == Status.failed_download): status_message = "The wallpaper failed to download"
-    elif(status == Status.no_suitable_url): status_message = "There were no suitable images that met the search criteria"
-    writer.write("Image_URI: %s\tStatus: %s\tDate: %s\tTime: %s" % (image_uri,status_message, time.strftime("%m/%d/%y"),time.strftime("%I:%M:%S")))
-    writer.close()
 
 def change_wallpaper(image_uri):
     filepath = "file://%s" % image_uri
@@ -75,6 +75,19 @@ def change_wallpaper(image_uri):
         return Status.changed_wallpaper
     except:
         return Status.unchanged_wallpaper
+
+def log(image_uri,savedir,status):
+    log_file = "%s/log.txt" % savedir
+    log_flag = "a" if os.path.isfile(log_file) else "w"
+    writer = open(log_file,log_flag)
+    status_message = "Something unexpected happened, please try again."
+    if(status == Status.changed_wallpaper): status_message = "The wallpaper has been changed successfully."
+    elif(status == Status.unchanged_wallpaper): status_message = "The wallpaper downloaded successfully but could not be set."
+    elif(status == Status.failed_download): status_message = "The wallpaper failed to download."
+    elif(status == Status.no_suitable_url): status_message = "There were no suitable images that met the search criteria."
+    elif(status == Status.bad_arguments): status_message = "The command line arguments were entered incorrectly."
+    writer.write("Image_URI: %s\tStatus: %s\tDate: %s\tTime: %s\n" % (image_uri,status_message, time.strftime("%m/%d/%y"),time.strftime("%I:%M:%S")))
+    writer.close()
 
 if __name__ == '__main__':
     main()
